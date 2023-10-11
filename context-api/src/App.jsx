8d4caf68c95ca8b1cd2b92aa898d043584bd3b44
neeraj-gs <gsneeraj05@gmail.com>
2023-10-11 09:48:24 +0530

    Added 4 COMpeonts that will either increemtn or decrement thte counter and now we need to use context api for them to render only specific part

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -1,7 +1,14 @@
+import Counter from "./components/Counter"
+
 function App() {
   return (
     <div className="App">
       <h1 style={{textAlign:"center"}}>Context API </h1>
+      <h5>Count is 0</h5>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   )
 }
---
+++ context-api/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div>
+      <button>Increemnt</button>
+      <button>Decremnt</button>
+    </div>
+  )
+}
+
+export default Counter
