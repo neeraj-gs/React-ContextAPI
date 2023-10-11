f875dc3dfe6e813781507422089cb795deba0d0f
neeraj-gs <gsneeraj05@gmail.com>
2023-10-11 09:59:02 +0530

    Added value prop that takes a state and tahat state is accessbile to all teh components

--- context-api/src/context/Counter.jsx
+++ context-api/src/context/Counter.jsx
@@ -1,12 +1,13 @@
 //this is a counter context
 
-import {createContext} from  'react'
+import {createContext,useState} from  'react'
 
 export const CounterContext = createContext(null); //create a coutner context and we can also specify some default value
-
+//anuthing inside value is accessible to all comoeonts
 export const CounterProvider=(props)=>{
+   const [count,setCount] = useState(0);
    return(
-      <CounterContext.Provider>
+      <CounterContext.Provider value={{count}}>
          {props.children}
       </CounterContext.Provider>
    )
